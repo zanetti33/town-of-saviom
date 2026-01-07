3aa26402411d8498c09e3319fbef52fd0e0ad40d
Fabio Pedrini <fabio.pedrini3@studio.unibo.it>
2026-01-07 18:13:10 +0100

    fix: merge conflicts typo

--- src/components/DashboardContainer.vue
+++ src/components/DashboardContainer.vue
@@ -6,33 +6,34 @@
                 <h1 class="main-title">Welcome, Player</h1>
             </div>
 
-        <button class="main-button" @click="logout">
-            Logout
-        </button>
-        <button class="main-button" @click="createRoom">
-            Create Room
-        </button>
-        <button class="main-button" @click="joinRoom">
-            Join Room
-        </button>
-        <button class="main-button"  @click="openOptions">
-            Options
-        </button>
-        <button class="main-button"  @click="showRules = true">
-            Rules
-        </button>
-        <div v-if="showRules" class="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
-            <div class="bg-gray-800 border-2 border-yellow-600 p-6 rounded-lg max-w-lg w-full text-white shadow-2xl">
-                <h2 class="text-2xl font-bold mb-4 text-yellow-500 underline">Game Rules</h2>
-                
-                <div class="max-h-96 overflow-y-auto mb-4 text-sm leading-relaxed">
-                    <pre class="font-sans whitespace-pre-wrap text-gray-200">{{ rulesContent }}</pre>
+            <button class="main-button" @click="logout">
+                Logout
+            </button>
+            <button class="main-button" @click="createRoom">
+                Create Room
+            </button>
+            <button class="main-button" @click="joinRoom">
+                Join Room
+            </button>
+            <button class="main-button"  @click="openOptions">
+                Options
+            </button>
+            <button class="main-button"  @click="showRules = true">
+                Rules
+            </button>
+            <div v-if="showRules" class="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
+                <div class="bg-gray-800 border-2 border-yellow-600 p-6 rounded-lg max-w-lg w-full text-white shadow-2xl">
+                    <h2 class="text-2xl font-bold mb-4 text-yellow-500 underline">Game Rules</h2>
+                    
+                    <div class="max-h-96 overflow-y-auto mb-4 text-sm leading-relaxed">
+                        <pre class="font-sans whitespace-pre-wrap text-gray-200">{{ rulesContent }}</pre>
+                    </div>
+                    <button 
+                        class="mt-4 bg-red-700 hover:bg-red-600 text-white font-bold py-2 px-4 rounded transition"
+                        @click="showRules = false">
+                        X
+                    </button>
                 </div>
-                <button 
-                    class="mt-4 bg-red-700 hover:bg-red-600 text-white font-bold py-2 px-4 rounded transition"
-                    @click="showRules = false">
-                    X
-                </button>
             </div>
         </div>
     </div>
