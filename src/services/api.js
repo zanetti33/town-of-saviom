34780bcb80731fbcfb11853266f45ff0ca2fa5e8
lzanetti <lorenzo.zanetti5@studio.unibo.it>
2026-01-11 12:26:53 +0100

    ci: configurazioni per deploy in produzione

--- src/services/api.js
+++ src/services/api.js
@@ -1,36 +1,23 @@
 import axios from 'axios';
 import { setupAuthInterceptors } from './authInterceptor';
-const LOGIN_API_URL = 'http://localhost:3000';
-const LOBBY_API_URL = 'http://localhost:3001';
-const GAMEPLAY_API_URL = 'http://localhost:3002';
-const STATS_API_URL = 'http://localhost:3003';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const LOGIN_API_URL = BASE_URL ? `${BASE_URL}/api/login` : 'http://localhost:3000';
+const LOBBY_API_URL = BASE_URL ? `${BASE_URL}/api/lobby` : 'http://localhost:3001';
+const GAMEPLAY_API_URL = BASE_URL ? `${BASE_URL}/api/gameplay` : 'http://localhost:3002';
+const STATS_API_URL = BASE_URL ? `${BASE_URL}/api/stats` : 'http://localhost:3003';
 
-const loginApi = axios.create({
-    baseURL: LOGIN_API_URL,
-    withCredentials: true
-});
+const createInstance = (url) => {
+    const instance = axios.create({
+        baseURL: url,
+        withCredentials: true
+    });
+    setupAuthInterceptors(instance);
+    return instance;
+};
 
-setupAuthInterceptors(loginApi);
-
-const lobbyApi = axios.create({
-    baseURL: LOBBY_API_URL,
-    withCredentials: true
-});
-
-setupAuthInterceptors(lobbyApi);
-
-const gameplayApi = axios.create({
-    baseURL: GAMEPLAY_API_URL,
-    withCredentials: true
-});
-
-setupAuthInterceptors(gameplayApi);
-
-const statsApi = axios.create({
-    baseURL: STATS_API_URL,
-    withCredentials: true
-});
-
-setupAuthInterceptors(statsApi);
+const loginApi = createInstance(LOGIN_API_URL);
+const lobbyApi = createInstance(LOBBY_API_URL);
+const gameplayApi = createInstance(GAMEPLAY_API_URL);
+const statsApi = createInstance(STATS_API_URL);
 
 export { loginApi, lobbyApi, gameplayApi, statsApi, LOGIN_API_URL, LOBBY_API_URL, GAMEPLAY_API_URL, STATS_API_URL };
