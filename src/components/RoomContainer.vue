c8651e4b38ad7d48d39c5bbd848fc7670daad0c7
lzanetti <lorenzo.zanetti5@studio.unibo.it>
2026-01-17 15:07:56 +0100

    fix: grafica di RoomContainer.vue

--- src/components/RoomContainer.vue
+++ src/components/RoomContainer.vue
@@ -4,9 +4,9 @@
             <h2 v-if="roomName" class="section-title">{{ roomName }}</h2>
 
             <!-- Pulsante Copia -->
-            <div class="subtitle flex mt-2 gap-2">
-                <label v-if="roomCode" class="form-label">Code: {{ roomCode }}</label>
-                <button @click="copyRoomCode" class="icon-button">
+            <div class="subtitle flex flex-row items-center gap-4">
+                <label class="text-primary">Room code: {{ roomCode }}</label>
+                <button v-if="roomCode" @click="copyRoomCode" class="form-button">
                     <svg v-if="!copied" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-background-5 group-hover:text-white transition-colors" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
                     </svg>
@@ -15,16 +15,9 @@
                     </svg>
                 </button>
             </div>
-            <div v-if="players.length<roomCapacity" class="backdrop-blur max-w-fit bg-light-primary border border-light-primary rounded-full shadow-2xl p-3 flex items-center gap-3">
-                <span class="w-3 h-3 rounded-full animate-ping"></span>
-                <span class="text-primary">Waiting for Players... ({{ players.length }}/{{ roomCapacity }})</span>
-            </div>
-            <div v-else class="backdrop-blur max-w-fit bg-light-blue border border-dark-primary rounded-full shadow-2xl p-3 flex items-center gap-2">
-                <span class="w-4 h-4 rounded-full animate-none"></span>
-                <span class="text-primary">Room is Full! ({{ players.length }}/{{ roomCapacity }})</span>
-            </div>
+            <WaitingPlayersLabel :ready-to-start="canStartGame" :players-number="players.length" :room-capacity="roomCapacity" />
             <PlayerCardList :players="players" />
-            <div class="flex flex-col md:flex-row gap-4">
+            <div class="w-full flex flex-col md:flex-row gap-4">
                 <button @click="onExitButtonClick" class="no-button">Exit</button>
                 <button 
                     v-if="isHost" 
@@ -48,6 +41,7 @@ import { io } from 'socket.io-client';
 import { useAuthStore } from '../stores/authStore';
 import router from '../router';
 import PlayerCardList from './PlayerCardList.vue';
+import WaitingPlayersLabel from './WaitingPlayersLabel.vue';
 
 export default {
     data() {
@@ -66,7 +60,8 @@ export default {
         };
     },
     components: {
-        PlayerCardList
+        PlayerCardList,
+        WaitingPlayersLabel
     },
     computed: {
         canStartGame() {
@@ -94,7 +89,7 @@ export default {
                 this.copied = true;
                 setTimeout(() => {
                     this.copied = false;
-                }, 2000);
+                }, 1000);
             } catch (err) {
                 console.error('Errore durante la copia:', err);
             }
---
+++ src/components/WaitingPlayersLabel.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-if="readyToStart" class="border-2 border-highlight p-2 rounded-full">
+        <span class="text-highlight">ready to start!</span>
+    </div>
+    <div v-else-if="playersNumber<roomCapacity" class="border-2 border-primary p-2 rounded-full">
+        <span class="text-primary">waiting for players... ({{ playersNumber }}/{{ roomCapacity }})</span>
+    </div>
+    <div v-else class="border-2 border-bad p-2 rounded-full">
+        <span class="text-bad">room is full! ({{ playersNumber }}/{{ roomCapacity }})</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        readyToStart: {
+            type: Boolean,
+            required: true
+        },
+        playersNumber: {
+            type: Number,
+            required: true
+        },
+        roomCapacity: {
+            type: Number,
+            required: true
+        }
+    }
+}
+</script>
