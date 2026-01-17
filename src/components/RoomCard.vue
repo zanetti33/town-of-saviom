99d2da5d9a2ab04d5703f3a037e60f73ba9ee662
lzanetti <lorenzo.zanetti5@studio.unibo.it>
2026-01-17 12:22:31 +0100

    fix: altri fix relativi ai colori

--- src/components/RoomCard.vue
+++ src/components/RoomCard.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="relative bg-background-3 border border-background-4 rounded-md p-4">
+    <div class="relative bg-card-background border border-border-background rounded-md p-4">
         <h3 class="font-bold">{{ room.name }}</h3>
         <p>#{{ room.code }}</p>
-        <p class="text-background-5">{{ room.gameMode }}</p>
-        <button class="submit-button w-30! absolute bottom-4 right-4" @click="joinRoom(room)">Join</button>
+        <p class="text-primary">{{ room.gameMode }}</p>
+        <button class="form-button absolute bottom-4 right-4" @click="joinRoom(room)">Join</button>
     </div>
 </template>
 
